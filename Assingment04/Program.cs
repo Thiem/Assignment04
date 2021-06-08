afa2b1731655055ac69913b433adf2f97fed5175
Thiem <tranthiem1993@gmail.com>
2021-06-08 18:44:55 +0700

    Asssignment04

---
+++ Assingment04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assingment04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Window[] window = new Window[3];
+            window[0] = new ListBox(50, 100, "Content of list box 1");
+            window[1] = new ListBox(200, 100, "Content of list box 2");
+            window[2] = new Button(30, 50);
+
+            for (int i = 0; i < 3; i++)
+            {
+                window[i].DrawWindow();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Assingment04/Window.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assingment04
+{
+    abstract class Window
+    {
+        public int top { get; set; }
+        public int left { get; set; }
+        public Window()
+        {
+
+        }
+        public Window(int top, int left)
+        {
+            this.top = top;
+            this.left = left;
+        }
+        public abstract void DrawWindow();
+    }
+}
